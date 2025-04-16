f0f6ffdb6596245de3ccf848de71d033612df9b2
ribhavsingla <ribhavsingla2166@gmail.com>
2025-04-17 00:19:48 +0530

    fixed silver layer

--- DATE_WAREHOUSE/scripts/silver/proc_load_silver.sql
+++ DATE_WAREHOUSE/scripts/silver/proc_load_silver.sql
@@ -147,11 +147,24 @@ BEGIN
 				WHEN sls_due_dt = 0 OR LENGTH(sls_due_dt) != 8 THEN NULL
 				ELSE CAST(CAST(sls_due_dt AS CHAR) AS DATE)
 			END AS sls_due_dt,
-			CASE 
-				WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price) 
-					THEN sls_quantity * ABS(sls_price)
-				ELSE sls_sales
-				END AS sls_sales,
+			CASE
+        		WHEN sls_sales IS NULL OR sls_sales <= 0 
+					OR sls_sales != sls_quantity * ABS(
+                 		CASE 
+                     		WHEN sls_price IS NULL OR sls_price <= 0 
+                         		THEN sls_sales / NULLIF(sls_quantity, 0)
+                     			ELSE sls_price
+                 		END
+            		)
+        			THEN sls_quantity * ABS(
+            			CASE 
+                			WHEN sls_price IS NULL OR sls_price <= 0 
+                    			THEN sls_sales / NULLIF(sls_quantity, 0)
+								ELSE sls_price
+						END
+					)
+					ELSE sls_sales
+			END AS sls_sales,
 			sls_quantity,
 			CASE 
 				WHEN sls_price IS NULL OR sls_price <= 0 
